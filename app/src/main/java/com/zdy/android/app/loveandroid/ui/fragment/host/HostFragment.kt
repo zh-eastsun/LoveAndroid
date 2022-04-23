99574a9b348d817a0d980db68fb09cf5513b587b
zhangdongyang <zh_eastsun@163.com>
2022-04-24 00:32:53 +0800

    [modified] 修改基础库中参数遍历逻辑
    Signed-off-by: zhangdongyang <zh_eastsun@163.com>

--- app/src/main/java/com/zdy/android/app/loveandroid/ui/fragment/host/HostFragment.kt
+++ app/src/main/java/com/zdy/android/app/loveandroid/ui/fragment/host/HostFragment.kt
@@ -1,11 +1,9 @@
 package com.zdy.android.app.loveandroid.ui.fragment.host
 
-import android.view.View
 import androidx.fragment.app.viewModels
-import androidx.viewpager.widget.ViewPager
 import com.zdy.android.app.loveandroid.BR
 import com.zdy.android.app.loveandroid.R
-import com.zdy.android.app.loveandroid.adapter.BannerAdapter
+import com.zdy.android.app.loveandroid.adapter.AdvertiseBannerAdapter
 import com.zdy.android.application.architecture.common.base.BaseFragment
 import com.zdy.android.application.architecture.common.base.binding.DataBindingConfig
 
@@ -25,10 +23,5 @@ class HostFragment : BaseFragment() {
             R.layout.fragment_host,
             BR.hostViewModel,
             hostViewModel
-        )
-
-    override fun initView(view: View) {
-        val viewpager = view.findViewById<ViewPager>(R.id.viewpager)
-        viewpager.adapter = BannerAdapter(listOf(1,2,3))
-    }
+        ).addBindingParam(BR.bannerAdapter, AdvertiseBannerAdapter())
 }
