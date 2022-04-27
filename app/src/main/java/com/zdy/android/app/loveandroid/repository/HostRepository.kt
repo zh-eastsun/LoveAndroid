e512811ae44de06be8bd1f0e62e4e73de1b80143
zhangdongyang <zhangdongyang@xiaomi.com>
2022-04-27 17:20:11 +0800

    [modified] 重构网络层

--- app/src/main/java/com/zdy/android/app/loveandroid/repository/HostRepository.kt
+++ app/src/main/java/com/zdy/android/app/loveandroid/repository/HostRepository.kt
@@ -1,6 +1,6 @@
 package com.zdy.android.app.loveandroid.repository
 
-import com.zdy.android.app.loveandroid.api.APIs
+import com.zdy.android.app.loveandroid.api.WanServices
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
@@ -16,16 +16,17 @@ import java.util.concurrent.TimeUnit
 object HostRepository {
 
     private val retrofit: Retrofit by lazy {
-        val logging = HttpLoggingInterceptor()
-        logging.level =HttpLoggingInterceptor.Level.BODY
-        val client = OkHttpClient.Builder()
-            .connectTimeout(8, TimeUnit.SECONDS)
-            .readTimeout(8, TimeUnit.SECONDS)
-            .writeTimeout(8, TimeUnit.SECONDS)
-            .addInterceptor(logging)
-            .build()
+        val logging = HttpLoggingInterceptor().apply { HttpLoggingInterceptor.Level.BODY }
+
+        val client = OkHttpClient.Builder().apply {
+            connectTimeout(8, TimeUnit.SECONDS)
+            readTimeout(8, TimeUnit.SECONDS)
+            writeTimeout(8, TimeUnit.SECONDS)
+            addInterceptor(logging)
+        }.build()
+
         Retrofit.Builder()
-            .baseUrl(APIs.BASE_URL)
+            .baseUrl(WanServices.BASE_URL)
             .client(client)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
